e1ebae03d2490a2c556b14acfe0f7d7afa4638a6
Algaba <89382004+Algaba84@users.noreply.github.com>
2023-02-20 19:54:02 +0000

    README generator with node

---
+++ index.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require('path');
+const generateMarkdown = require("./utils/generateMarkdown");
+
+//console.log('readme')
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project called?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a short description of the project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instruction:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contributions guidelines:',
+      },
+      {
+          type: 'input',
+          name: 'test',
+          message: 'Enter instruction to test:',
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: 'For questions(github):',
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'For questions(email):',
+        },
+        {
+          type: 'list',
+          name: 'license',
+          message: 'Choose your license:',
+          choices: ['MIT', 'ISC', 'GNUPLv3'],
+        }
+
+];
+
+
+// function to initialize program
+function init() {
+  return inquirer.prompt(questions)
+  .then((answers)=>{
+    const markDown = generateMarkdown(answers)
+    
+    //console.log(markDown)
+    //console.log(answers)
+    //return answers
+    fs.writeFile(path.join(process.cwd() + '/dist/', 'README.md'), markDown, (err) => {
+           if(err){
+            console.log('Could not save file')
+           } else {
+            console.log('Succes: new README.md file generated')
+           }
+    })
+    
+  })
+ 
+}
+
+// function call to initialize program
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,57 @@
+
+// function to generate logo licence
+
+let renderLicence = (license) => {
+  switch (license) {
+    case "MIT":
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case "ISC":
+      return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+    case "GNUPLv3":
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  }
+}
+
+// function to generate markdown for README
+function generateMarkdown(data) {
+    return `
+# ${data.title}
+${renderLicence(data.license)}
+## Description
+  ${data.description}
+
+##  Table of Content
+- [Project description](#description)
+- [Installation](#installation)
+- [Instruction](#instruction)
+- [Usage](#usage)
+- [Constributions](#contributing)
+- [TestInstruction](#test)
+- [Questions](#questions)
+- [Lincence](#licence)
+  
+## Installation
+  ${data.installation}
+
+## Usage
+  ${data.usage}
+
+## Constributing
+  ${data.contributing}
+
+## Test 
+  ${data.test}
+
+## Questions
+  - Github: [@${data.github}](https://github.com/${data.github})
+  
+  - Email: ${data.email}
+   
+## License
+  ${data.license}
+  `;
+  }
+  
+  
+  module.exports = generateMarkdown;
+  
